241e16f07373ef43176c30744d52a8399c25002f
Peter Christensen <peter@puptel.net>
2019-11-23 19:31:26 -0500

    Revert OperationalError check.

--- ipset/apps.py
+++ ipset/apps.py
@@ -8,13 +8,17 @@ class IpsetConfig(AppConfig):
     name = 'ipset'
     verbose_name = 'IP set'
 
+    # pylint: disable=import-outside-toplevel
     def ready(self):
         """ Sync and clean IP sets when the project is ready. Send
         admin email on failure. """
+        from django.db.utils import OperationalError
         from ipset.maintenance import sync
 
         try:
             sync()
+        except OperationalError:
+            pass  # This happens when migrating for the first time.
         except ValueError as err:
             import logging
             logging.getLogger('django.ipset').error(str(err))
